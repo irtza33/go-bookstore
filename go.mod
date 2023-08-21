ab64c89cafd4c108d35e8afcf8678c0d9d2b617d
irtza33 <irtzatariq23@gmail.com>
2023-08-22 01:24:04 +0500

    backend routing

--- go.mod
+++ go.mod
@@ -1,5 +1,13 @@
-module github.com/irtza33/go-bookstore
+module github.com/irtza33/go-bookstore/tree/main
 
 go 1.21.0
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/jinzhu/gorm v1.9.16
+)
+
+require (
+	github.com/go-sql-driver/mysql v1.5.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+)
--- pkg/routes/bookstore-routes.go
+++ pkg/routes/bookstore-routes.go
@@ -2,11 +2,13 @@ package routes
 
 import (
 	"github.com/gorilla/mux"
-	"github.com/irtza33/go-bookstore/pkg/controllers"
+	"github.com/irtza33/go-bookstore/tree/main/pkg/controllers"
 )
 
 var RegisterBookStoreRoutes = func(router *mux.Router) {
 	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
 	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
 	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
+	//router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("book/{bookId}", controllers.DeleteBook).Methods("DELETE")
 }
