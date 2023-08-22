8c8524f5dfcd1c15e4ea97dcfd52f72108e9e96c
irtza33 <irtzatariq23@gmail.com>
2023-08-22 18:25:18 +0500

    All APIs completed

--- pkg/controllers/book-controllers.go
+++ pkg/controllers/book-controllers.go
@@ -2,8 +2,11 @@ package controllers
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"strconv"
 
+	"github.com/gorilla/mux"
 	"github.com/irtza33/go-bookstore/tree/pure_sql/pkg/models"
 	"github.com/irtza33/go-bookstore/tree/pure_sql/pkg/utils"
 )
@@ -15,4 +18,47 @@ func CreateBook(w http.ResponseWriter, r *http.Request) {
 	res, _ := json.Marshal(b)
 	w.WriteHeader(http.StatusOK)
 	w.Write((res))
+}
+
+func GetBook(w http.ResponseWriter, r *http.Request) {
+	newBooks := models.GetAllBooks()
+	res, _ := json.Marshal(newBooks)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader((http.StatusOK))
+	w.Write((res))
+}
+
+func GetBookById(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	Id, err := strconv.ParseInt(bookId, 10, 0)
+	if err != nil {
+		panic(err)
+	}
+	fetchedBook := models.GetBookById(Id)
+	res, _ := json.Marshal(fetchedBook)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func DeleteBook(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	Id, err := strconv.ParseInt(bookId, 10, 0)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(Id)
+	if models.DeleteBook(Id) {
+		res, _ := json.Marshal("Book has been deleted")
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(res)
+	} else {
+		res, _ := json.Marshal("Book could not be deleted")
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadGateway)
+		w.Write(res)
+	}
 }
--- pkg/models/book.go
+++ pkg/models/book.go
@@ -8,6 +8,7 @@ import (
 )
 
 type Book struct {
+	Id          int
 	Name        string
 	Author      string
 	Publication string
@@ -18,6 +19,9 @@ var DB *sql.DB
 func init() {
 	config.Connect()
 	DB = config.GetDB()
+	if _, err := DB.Exec(`CREATE TABLE IF NOT EXISTS books(Id INT NOT NULL AUTO_INCREMENT, Name VARCHAR(25), Author VARCHAR(25), Publication VARCHAR(25), PRIMARY KEY (Id));`); err != nil {
+		panic(err)
+	}
 }
 
 func (b *Book) CreateBook() *Book {
@@ -26,4 +30,43 @@ func (b *Book) CreateBook() *Book {
 		panic(err)
 	}
 	return b
+}
+
+func GetAllBooks() []Book {
+	var Books []Book
+	var TempBook Book
+	rows, err := DB.Query(`SELECT * FROM books`)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer rows.Close()
+	for rows.Next() {
+		rows.Scan(&TempBook.Id, &TempBook.Name, &TempBook.Author, &TempBook.Publication)
+		Books = append(Books, TempBook)
+	}
+	return Books
+}
+
+func GetBookById(Id int64) *Book {
+	var fetchedBook Book
+	rows, err := DB.Query(`SELECT * FROM books WHERE Id = ?;`, Id)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		if err := rows.Scan(&fetchedBook.Id, &fetchedBook.Name, &fetchedBook.Author, &fetchedBook.Publication); err != nil {
+			panic(err.Error())
+		}
+
+	}
+	return &fetchedBook
+}
+
+func DeleteBook(Id int64) bool {
+	_, err := DB.Exec(`DELETE FROM books WHERE Id = ?`, Id)
+	if err != nil {
+		panic(err)
+	}
+	return true
 }
